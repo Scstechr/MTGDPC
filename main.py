09ad667625228bfe9a0113cf7ca84c8acc2f3739
Scstechr <teufelkonig@gmail.com>
2018-11-29 05:33:14 +0900

    defining main function

--- main.py
+++ main.py
@@ -19,45 +19,48 @@ def deck(string):
     deck = string.split('\n')
     return [Card(card) for card in deck]
 
-from multiprocessing import Pool
-import multiprocessing as multi
+def proc(d):
+    from multiprocessing import Pool
+    import multiprocessing as multi
 
-maxproc = multi.cpu_count()
+    maxproc = multi.cpu_count()
 
-m_dlist = []
-s_dlist = []
+    with Pool(processes=maxproc) as p:
+       lst = p.map(search, d)
 
-with open(deckname, 'r') as rf:
-    main, side = ''.join(r for r in rf).split('\n\nSideboard\n')
+    return lst
 
-main, side = deck(main), deck(side[:-1])
+def main():
+    with open(deckname, 'r') as rf:
+        main, side = ''.join(r for r in rf).split('\n\nSideboard\n')
 
-with Pool(processes=maxproc) as p:
-   m_dlist = p.map(search, main)
+    main, side = deck(main), deck(side[:-1])
 
-with Pool(processes=maxproc) as p:
-   s_dlist = p.map(search, side)
+    m_dlist = proc(main)
+    s_dlist = proc(side[:-1])
 
-print("\n------------------------------MAIN DECK----------------------------\n")
-total = 0
-for card, d in zip(main, m_dlist):
-    price = int(d['price'].replace(',',''))
-    num = int(card.num)
-    total += price * num
-    print(f"{price} x {num} = {price*num}".ljust(20), end=' ')
-    print(d['name'])
+    print("\n------------------------------MAIN DECK----------------------------\n")
+    total = 0
+    for card, d in zip(main, m_dlist):
+        price = int(d['price'].replace(',',''))
+        num = int(card.num)
+        total += price * num
+        print(f"{price} x {num} = {price*num}".ljust(20), end=' ')
+        print(d['name'])
 
-print("\nmain ) price:", total)
+    print("\nmain ) price:", total)
 
-print("\n------------------------------SIDEBOARD----------------------------\n")
-s_total = 0
-for card, d in zip(side, s_dlist):
-    price = int(d['price'].replace(',',''))
-    num = int(card.num)
-    s_total += price * num
-    print(f"{price} x {num} = {price*num}".ljust(20), end=' ')
-    print(d['name'])
+    print("\n------------------------------SIDEBOARD----------------------------\n")
+    s_total = 0
+    for card, d in zip(side, s_dlist):
+        price = int(d['price'].replace(',',''))
+        num = int(card.num)
+        s_total += price * num
+        print(f"{price} x {num} = {price*num}".ljust(20), end=' ')
+        print(d['name'])
 
-print("\nside ) price:", s_total)
-print("\ntotal price (main + side):", total + s_total)
+    print("\nside ) price:", s_total)
+    print("\ntotal price (main + side):", total + s_total)
 
+if __name__ == "__main__":
+    main()
